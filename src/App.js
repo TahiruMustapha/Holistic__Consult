cbffcdac5070c93c1785af6eea1488b97507cbe8
TahiruMustapha <tahirumustapha13@gmail.com>
2023-01-08 10:55:48 +0000

    Added some files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import About from './components/About/About';
+import Service from './components/service/Service';
+import Contact from './components/contact/Contact';
+import Lost from './components/404/Lost';
+import Appointment from './components/appointment/Appointment';
+import { useState } from 'react';
+import Apply from './components/apply/Apply';
+import Events from './components/events/Events';
+function App({second}) {
+   const[openAppointment,setOpenAppointment] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tahiru Mustapha I.T Services
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+   <BrowserRouter>
+    <div>
+      <NavBar  second = {setOpenAppointment}/>
+      <Routes>
+         <Route path='/' exact={true} element = { <Home homeBtn = {()=>setOpenAppointment(true)} onClose = {()=>setOpenAppointment(false)}/> }/>
+         <Route path='/about' exact element = { <About aboutBtn = {setOpenAppointment}/> }/>
+         <Route path='/service' exact element = { <Service ServiceBtn ={setOpenAppointment}/> }/>
+         <Route path='/contact' exact element = { <Contact/> }/>
+         <Route path='/events' exact element = { <Events/> }/>
+         <Route path='/apply'exact element = { <Apply/>}/>
+        <Route path='*' exact element = { <Lost/> }/>
+      </Routes>
+      <Appointment open = {openAppointment} onClose = {()=>setOpenAppointment(false)} />
+      <Footer/>
     </div>
+   </BrowserRouter>
   );
 }
 
