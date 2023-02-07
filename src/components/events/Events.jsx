4c143b4e4d9eb85ccd2be791438719f500eca88d
TahiruMustapha <tahirumustapha13@gmail.com>
2023-02-07 02:15:17 +0000

    made changes

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -7,15 +7,15 @@ const Events = () => {
          <h1>Upcoming Events</h1>
          <div className='eventsDetails'>
             <div>
-               <p className='day'>SUNDAY</p>
-               <p className='date'>JAN  <span>1st</span></p>
+               <p className='day'>STUDY</p>
+               <p className='date'>JAN,  <span>2023 </span>(6 months)</p>
                <p className='modules-text'>Apply to lay practitioner certificate in pastoral counselling today!</p>
                <p className='modules'>4 modules in 6months.</p>
                <Link to='/apply'><button className='events-btn'>Apply</button></Link>
             </div>
             <div>
-            <p className='day'>SUNDAY</p>
-            <p className='date'>JAN <span>20th</span></p>
+            <p className='day'>STUDY</p>
+            <p className='date'>JAN,  <span>2023 </span>(6 months)</p>
             <p className='modules-text'>Apply to moral injury dimension and their treatment modules today!</p>
             <p className='modules'>2 Day practical workshop.</p>
             <Link to='/apply'><button className='events-btn'>Apply</button></Link>
