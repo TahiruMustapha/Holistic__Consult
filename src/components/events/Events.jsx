809f52c30a77424caf3d53ab76e3902cea8e2a24
TahiruMustapha <tahirumustapha13@gmail.com>
2023-02-25 10:17:37 +0000

    made changes

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -7,15 +7,17 @@ const Events = () => {
          <h1>Upcoming Events</h1>
          <div className='eventsDetails'>
             <div>
-               <p className='day'>STUDY</p>
-               <p className='date'>JAN,  <span>2023 </span>(6 months)</p>
+               <p className='day'>Weekday: Mon-Thur</p>
+               <p className="day-time">9:00am - 2:00pm</p>
+               <p className='date'>APRIL,  <span>2023 </span>(4 months)</p>
                <p className='modules-text'>Apply to lay practitioner certificate in pastoral counselling today!</p>
-               <p className='modules'>4 modules in 6months.</p>
+               <p className='modules'>4 modules in 4months.</p>
                <Link to='/apply'><button className='events-btn'>Apply</button></Link>
             </div>
             <div>
-            <p className='day'>STUDY</p>
-            <p className='date'>JAN,  <span>2023 </span>(6 months)</p>
+            <p className='day'>Weekday: Mon-Thur</p>
+               <p className="day-time">9:00am - 2:00pm</p>
+            <p className='date'>APRIL,  <span>2023 </span>(4 months)</p>
             <p className='modules-text'>Apply to moral injury dimension and their treatment modules today!</p>
             <p className='modules'>2 Day practical workshop.</p>
             <Link to='/apply'><button className='events-btn'>Apply</button></Link>
